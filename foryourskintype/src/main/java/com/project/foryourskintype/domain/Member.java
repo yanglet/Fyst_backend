11162a7e1730fa6fe9b05fb42747d8764e2dee55
양충욱 <96788792+yanglet@users.noreply.github.com>
2022-01-26 16:49:18 +0900

    Update Member.java

--- foryourskintype/src/main/java/com/project/foryourskintype/domain/Member.java
+++ foryourskintype/src/main/java/com/project/foryourskintype/domain/Member.java
@@ -13,7 +13,6 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
     private String name;
-    private String price;
     private int age;
     private String gender;
     private String email;
