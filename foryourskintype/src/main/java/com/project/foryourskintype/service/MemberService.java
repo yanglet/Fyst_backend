2754633ac6ea77de8c7a9a544436bb251084aa56
양충욱 <96788792+yanglet@users.noreply.github.com>
2022-02-01 01:08:29 +0900

    Delete foryourskintype directory

--- foryourskintype/src/main/java/com/project/foryourskintype/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.project.foryourskintype.service;
-import com.project.foryourskintype.domain.Member;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface MemberService {
-    Long join(Member member);
-    int Login(String email, String password, HttpServletRequest request);
-}
